0703dd45ef81c8c7eb47d87dfa3ee38200ce7c02
Anderson Assum <116601450+AssumAnd@users.noreply.github.com>
2026-02-26 21:29:12 -0300

    Add files via upload

---
+++ crud_jdbc/Servidor.java
@@ -0,0 +1,14 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Servidor {
+    public static void main (String[] args)throws  IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/usuarios", new UsuarioController());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Servidor rodando em http://localhost:8080/usuarios");
+
+    }
+}
